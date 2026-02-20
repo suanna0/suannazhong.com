50945df5338d15e02b94b89f8b8a112b12a4a1da
suanna0 <117539182+suanna0@users.noreply.github.com>
2026-02-20 15:08:07 -0500

    reel

--- src/routes/reel/+page.svelte
+++ src/routes/reel/+page.svelte
@@ -4,27 +4,25 @@
 <svelte:head>
     <title>2025 Demo Reel | Suanna Zhong</title>
 </svelte:head>
-<iframe src="https://www.youtube.com/embed/IF2pvnG15vA?si=Usan5sya9ZosDIK_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<iframe src="https://www.youtube.com/embed/V_lHpIZ3A0k?si=KUhhqqeDFLPQAmRN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 <p>
-00:00 - isid or Blasting Sophie Music Video, Co-director, DP, Editor, Colorist 
+00:00 - Color Grading Work (Dehancer Partnership) 
 <br>
-00:03 - Frame-by-frame animation created in Procreate
+00:06 - Custom Google MediaPipe to TouchDesigner OSC Graphical Interface for Ceci Sun's Senior Thesis Dance Performance
 <br>
-00:11 - Malloc Visualizer, created animated visuals for the Introduction to Computer Systems course assignment, <code>malloclab</code>, required for all CS majors at CMU
+00:14 - Sculpted and animated asset for short film
 <br>
-00:16 - Lyric videos for Universal Music Group  
+00:19 - Lyric videos for Universal Music Group
 <br>
-00:24 - Sculpted and animated asset for short film
+00:25 - Web Development for 2026 Lunar Gala Website
 <br>
-00:30 - Technical character animation practice, rigs from <a target = "_blank" href = "https://dopesheet.andrew.cmu.edu/resources#h.42qaipdqrn4t">CMU Dopesheet</a>
+00:32 - Malloc Visualizer, created animated visuals for the Introduction to Computer Systems course assignment, <code>malloclab</code>, required for all CS majors at CMU
 <br>
-00:36 - Maya stage render for 2025 Lunar Gala Fashion Show
+00:39 - Creative Coding Projects 
 <br>
-00:48 - Dehancer paid partnership 
+00:50 - Frame-by-frame animations created in Procreate
 <br>
-00:51 - Realtree Lesbians, Director, DP, editor, composer, and colorist for queer short film. Featured in 2024 Ovation Film Fest
-<br>
-00:55 - February Pt. I, Director, DP, Editor, Colorist
+00:54 - Technical character animation practice, rigs from <a target = "_blank" href = "https://dopesheet.andrew.cmu.edu/resources#h.42qaipdqrn4t">CMU Dopesheet</a>
 </p>
 
 <PrevNext

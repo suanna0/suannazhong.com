aa46e37885b8adefa4e3a97e687cbaeef10390c1
suanna0 <117539182+suanna0@users.noreply.github.com>
2026-04-17 18:16:40 -0400

    grammar

--- src/routes/ccc/+page.svelte
+++ src/routes/ccc/+page.svelte
@@ -104,7 +104,7 @@
 </div>
 <h2>Inspiration</h2>
 <p class="two-col">
-    Previous works that create extensions of the human body include Umberto Boccioni’s Unique Forms of Continuity in Space (1913), Oskar Schlemmer’s Stelzenläufer (Slat Dance) (1927), and Zachary Lieberman’s 2016 sketch.
+    Previous works that create extensions of the human body include Umberto Boccioni's Unique Forms of Continuity in Space (1913), Oskar Schlemmer's Stelzenläufer (Slat Dance) (1927), and Zachary Lieberman's daily sketches.
     I was heavily inspired by <a target="_blank" href="https://youtu.be/s_S3fomiXO0?si=M4i9_uZZ90DdhHuP">discrete figures</a> by Daito Manabe's Rhizomatiks Research group,
     as well as Lingdong Huang's <a target="_blank" href="https://studioforcreativeinquiry.org/project/shan-shui">{'{'}Shan, Shui{'}'}*</a>,
     which Golan had previously shown us and undeniably influenced how we approached the mountain visuals.

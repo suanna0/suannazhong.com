407e6c3516f2fe5f86cc9814d00552bf35fe7c65
suanna0 <117539182+suanna0@users.noreply.github.com>
2026-05-08 19:09:41 -0400

    alumni email

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -69,7 +69,7 @@
                 Instagram
                 </a>
                 <br />
-                suannaz [at] andrew [dot] cmu [dot] edu
+                suannaz [at] alumni [dot] cmu [dot] edu
             </p>
             <h2>Selected Clients:</h2>
             <p>Adolescent Content, Akira Uchida, aron!, BonBonBardot!, Capitol Records, Charlie White, Chxrry, Common Sense Media, DAY6, Dehancer, Frankie Grande, Haiden Henderson, Jeremy Zucker, Jillian Jacqueline, JYP Entertainment, Kali Uchis, Kid Cudi, Lang Lang, Marcus Rahauser, Meta, Pinterest, Republic Records, Stray Kids, Virgin Music Group, VSCO, & more</p>
